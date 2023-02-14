2808bae2fa17092b49f968afc30b6c5182df93da
kenanxasiyevv <kananakh@code.edu.az>
2023-02-14 22:43:20 +0400

    Add project files.

---
+++ ImageEdit/ImageProcessing.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace ImageEdit
+{
+    internal class ImageProcessing
+    {
+        internal Bitmap ChangePixelColor(Bitmap image, int colorR, int colorG, int colorB)
+        {
+            int r, g, b;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color pixelColor = image.GetPixel(i, j);
+
+                    r = pixelColor.R + colorR;
+                    if (r > 255)
+                    {
+                        r = r % 255;
+                    }
+
+                    g = pixelColor.G + colorG;
+                    if (g > 255)
+                    {
+                        g = g % 255;
+                    }
+
+                    b = pixelColor.B + colorB;
+                    if (b > 255)
+                    {
+                        b = b % 255;
+                    }
+
+                    Color newColor = Color.FromArgb(r, g, b);
+
+                    image.SetPixel(i, j, newColor);
+                }
+            }
+
+            return image;
+        }
+        internal void SaveFile(Bitmap image)
+        {
+            Console.Write("Enter new file name: ");
+            string newImage = Console.ReadLine() + ".jpg";
+
+            image.Save(newImage);
+
+            Console.WriteLine("Image saved successfully");
+        }
+    }
+}
---
+++ ImageEdit/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Drawing;
+
+namespace ImageEdit
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageProcessing= new ImageProcessing();
+           
+            string key = "";
+            while (key != "quit")
+            {
+
+                Console.Write("enter path:");
+                string imagePath = (Console.ReadLine());
+                Bitmap image = new Bitmap(imagePath);
+
+
+                Console.WriteLine($"1.Random Filter\n2.Custom Filter");
+
+            start:
+                key = Console.ReadLine();
+
+
+                switch (key)
+                {
+                    case "1":
+                        var random = new Random();
+                        int r = random.Next(0, 255);
+                        int g = random.Next(0, 255);
+                        int b = random.Next(0, 255);
+                        Bitmap newRandmImage = imageProcessing.ChangePixelColor(image, r, g, b);
+                        imageProcessing.SaveFile(newRandmImage);
+                        image.Dispose();
+                        break;
+                    case "2":
+                        Console.Write("R:");
+                        int colorR = int.Parse(Console.ReadLine());
+                        Console.Write("G:");
+                        int colorG = int.Parse(Console.ReadLine());
+                        Console.Write("B:");
+                        int colorB = int.Parse(Console.ReadLine());
+                        Bitmap newImage = imageProcessing.ChangePixelColor(image, colorR, colorG, colorB);
+                        imageProcessing.SaveFile(newImage);
+                        image.Dispose();
+                        break;
+                    default:
+                        Console.WriteLine("switch correct case:");
+                        goto start;
+
+                }
+            }
+        }
+    }
+} 
